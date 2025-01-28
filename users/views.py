6c4061b12ce97d4251b40989b277171dbe7fadbe
Qilan Lin <78576883+QilanLin@users.noreply.github.com>
2025-01-28 06:35:25 +0000

    ibid

--- users/views.py
+++ users/views.py
@@ -4,6 +4,12 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 from .forms import CustomUserCreationForm
 
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from clubs.models import ClubRequest
+from clubs.models import Membership  # 导入 Membership 模型
+
+
 User = get_user_model()
 
 def register(request):
@@ -22,3 +28,23 @@ def register(request):
 def user_center_view(request):
     """用户中心视图"""
     return render(request, 'users/user_center.html')
+
+
+@login_required
+def club_requests_view(request):
+    """显示所有社团创建请求，仅供管理员访问"""
+    if not request.user.is_superuser and request.user.role != 'admin':
+        return render(request, '403.html', status=403)  # 返回无权限页面
+
+    club_requests = ClubRequest.objects.all()
+    return render(request, 'users/club_requests.html', {'club_requests': club_requests})
+
+
+
+
+@login_required
+def my_clubs_view(request):
+    """显示用户参加的所有社团"""
+    memberships = Membership.objects.filter(user=request.user)
+    clubs = [membership.club for membership in memberships]
+    return render(request, 'users/my_clubs.html', {'clubs': clubs})
