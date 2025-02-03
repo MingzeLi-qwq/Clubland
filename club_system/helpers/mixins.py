e5ba842cc2c7df76ea0b9573c6de9eb3c258f439
MingzeLi-qwq <mingzeli.qwq@gmail.com>
2025-02-03 13:06:14 +0000

    Initial completion of club system mixins初步完成了club_system的mixins

---
+++ club_system/helpers/mixins.py
@@ -0,0 +1,80 @@
+from django.contrib.auth.mixins import AccessMixin
+from django.shortcuts import redirect
+from django.http import HttpResponse
+from club_system.models import Membership
+from django.utils.safestring import mark_safe
+
+class ClubMemberRequiredMixin(AccessMixin):
+    """Access is limited to club members only"""
+    """只允许社团成员访问"""
+    def dispatch(self, request, *args, **kwargs):
+        club_id = kwargs.get('club_id')
+        if not Membership.objects.filter(user=request.user, club_id=club_id).exists():
+            message = """
+            <html>
+            <head>
+                <meta charset="UTF-8">
+                <script>
+                    setTimeout(function() {
+                        window.location.href = '/';
+                    }, 3000);  // Jump in 3 seconds / 3秒后跳转
+                </script>
+            </head>
+            <body>
+                <h2 style="text-align:center; margin-top:20%;">❌ 你不是该社团成员，无法访问此页面</h2>
+                <p style="text-align:center;">即将跳转到首页...</p>
+            </body>
+            </html>
+            """
+            return HttpResponse(mark_safe(message))  # mark_safe 让 HTML 代码生效
+        return super().dispatch(request, *args, **kwargs)
+
+class ClubAdminRequiredMixin(AccessMixin):
+    """Only allow access to club administrators"""
+    """只允许社团管理员访问"""
+    def dispatch(self, request, *args, **kwargs):
+        club_id = kwargs.get('club_id')
+        if not Membership.objects.filter(user=request.user, club_id=club_id, is_admin=True).exists():
+            message = """
+            <html>
+            <head>
+                <meta charset="UTF-8">
+                <script>
+                    setTimeout(function() {
+                        window.location.href = '/';
+                    }, 3000);  // Jump in 3 seconds / 3秒后跳转
+                </script>
+            </head>
+            <body>
+                <h2 style="text-align:center; margin-top:20%;">❌ 你不是该社团管理员，无法访问此页面</h2>
+                <p style="text-align:center;">即将跳转到首页...</p>
+            </body>
+            </html>
+            """
+            return HttpResponse(mark_safe(message))  # mark_safe 让 HTML 代码生效
+        return super().dispatch(request, *args, **kwargs)
+
+class NonClubMemberRequiredMixin(AccessMixin):
+    """Access is only allowed to non-members of the association"""
+    """只允许非社团成员访问"""
+    def dispatch(self, request, *args, **kwargs):
+        club_id = kwargs.get('club_id')
+        if Membership.objects.filter(user=request.user, club_id=club_id).exists():
+            message = """
+            <html>
+            <head>
+                <meta charset="UTF-8">
+                <script>
+                    setTimeout(function() {
+                        window.location.href = '/';
+                    }, 3000);  // Jump in 3 seconds / 3秒后跳转
+                </script>
+            </head>
+            <body>
+                <h2 style="text-align:center; margin-top:20%;">❌ 你已经是该社团成员，无法访问此页面</h2>
+                <p style="text-align:center;">即将跳转到首页...</p>
+            </body>
+            </html>
+            """
+            return HttpResponse(mark_safe(message))  # mark_safe 让 HTML 代码生效
+        return super().dispatch(request, *args, **kwargs)
