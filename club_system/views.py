00ceb21bff367e57391f0f01cac33ff37b33fc0e
MingzeLi-qwq <mingzeli.qwq@gmail.com>
2025-02-03 21:48:47 +0000

    On the club detail page you can now join the club! / 在club detail页面现在可以加入club了

--- club_system/views.py
+++ club_system/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
-from .models import Club
-from .helpers.mixins import ClubExistsRequiredMixin
+from .models import Club, Membership
+from .helpers.mixins import ClubExistsRequiredMixin, NonClubMemberRequiredMixin, ClubMemberRequiredMixin
 
 def clubs(request):
     clubs = Club.objects.all()
@@ -12,3 +12,21 @@ class ClubDetailView(ClubExistsRequiredMixin, View):
     def get(self, request, club_id, *args, **kwargs):
         club = Club.objects.get(pk=club_id)
         return render(request, 'club_detail.html', {'club': club})
+    
+class RegisterMembershipView(ClubExistsRequiredMixin, NonClubMemberRequiredMixin, View):
+    def get(self, request, club_id, *args, **kwargs):
+        club = Club.objects.get(pk=club_id)
+        if request.user.is_authenticated:
+            Membership.objects.get_or_create(user=request.user, club=club)
+            return redirect('club_detail', club_id=club_id)
+        else:
+            return redirect('login')
+        
+class CancelMembershipView(ClubExistsRequiredMixin, ClubMemberRequiredMixin, View):
+    def get(self, request, club_id, *args, **kwargs):
+        club = Club.objects.get(pk=club_id)
+        if request.user.is_authenticated:
+            Membership.objects.filter(user=request.user, club=club).delete()
+            return redirect('club_detail', club_id=club_id)
+        else:
+            return redirect('login')
--- student_union/urls.py
+++ student_union/urls.py
@@ -41,5 +41,6 @@ urlpatterns = [
     # Club related / Club相关页面
     path('clubs/', club_system.views.clubs, name='clubs'),
     path('clubs/detail/<int:club_id>/', club_system.views.ClubDetailView.as_view(), name='club_detail'),
-
+    path('clubs/detail/register_membership/<int:club_id>/', club_system.views.RegisterMembershipView.as_view(), name='register_membership'),
+    path('clubs/detail/cancel_membership/<int:club_id>/', club_system.views.CancelMembershipView.as_view(), name='cancel_membership'),
 ]
