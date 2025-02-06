3e7612952987a8792a7e9678a1edbf270ce832ba
DavidYouyouWu <k21185211@kcl.ac.uk>
2025-02-06 22:53:02 +0000

    Test for change password, add comments

--- notification_system/views.py
+++ notification_system/views.py
@@ -3,16 +3,19 @@ from django.shortcuts import render, get_object_or_404
 from .models import Notification
 
 def notification_list(request):
+    '''获取当前用户的所有通知'''
     notifications = Notification.objects.filter(user=request.user).order_by('-created_at')
     return render(request, 'notification_system/notifications.html', {'notifications': notifications})
 
 def notification_detail(request, notification_id):
+    '''访问单条通知，并将其标记为已读'''
     notification = get_object_or_404(Notification, id=notification_id, user=request.user)
     notification.is_read = True
     notification.save()
     return render(request, 'notification_system/notification_detail.html', {'notification': notification})
 
 def base_notifications(request):
+    '''在 base.html 里使用全局上下文加载未读通知'''
     notifications = Notification.objects.filter(user=request.user, is_read=False).order_by('-created_at')[:5]
     unread_notifications_count = notifications.count()
     return {
