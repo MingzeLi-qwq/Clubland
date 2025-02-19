812dd0ea2f2a3da35f82636de28acaae9dbec727
MingzeLi-qwq <mingzeli.qwq@gmail.com>
2025-02-19 16:09:01 +0000

    Added shadow to the card / 给卡片添加了影子

--- admin_system/views.py
+++ admin_system/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views import View
+from django.db.models import Q
 from user_system.helpers.mixins import LoginRequiredMixin, UserTypeRequiredMixin
+from club_system.models import Club, Membership, NewClubRequest
+from user_system.models import User
 
 
 """以下内容负责渲染Admin Panel"""
@@ -8,7 +11,18 @@ class AdminPanelClubs(LoginRequiredMixin, UserTypeRequiredMixin, View):
     allowed_types = ['Admin']
 
     def get(self, request, *args, **kwargs):
-        return render(request, 'admin_panel/clubs.html')
+        search_query = request.GET.get('search', '')
+        if search_query:
+            clubs = Club.objects.filter(Q(name__icontains=search_query) | Q(description__icontains=search_query))
+        else:
+            clubs = Club.objects.all()
+        
+        club_count = clubs.count()
+        return render(request, 'admin_panel/clubs.html', {
+            'clubs': clubs,
+            'club_count': club_count,
+            'search_query': search_query,
+        })
     
 class AdminPanelUsers(LoginRequiredMixin, UserTypeRequiredMixin, View):
     allowed_types = ['Admin']
