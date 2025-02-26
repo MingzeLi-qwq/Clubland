ee8abbcc1c8f8b558b381136ff8bfe30c08dc876
MingzeLi-qwq <mingzeli.qwq@gmail.com>
2025-02-26 17:45:37 +0000

    Implemented a search box for club members in the admin panel. / 在admin panel中的club members里实现了搜索框

--- admin_system/views.py
+++ admin_system/views.py
@@ -100,18 +100,39 @@ class AdminPanelClubsMembers(LoginRequiredMixin, UserTypeRequiredMixin, View):
     allowed_types = ['Admin']
     def get(self, request, club_id, *args, **kwargs):
         club = Club.objects.get(pk=club_id)
+        
+        # Get search parameters
+        manager_search = request.GET.get('manager_search', '')
+        member_search = request.GET.get('member_search', '')
+        
+        # Manager Enquiries
         managers = club.membership_set.filter(is_manager=True)
-        manager_count = managers.count()
+        if manager_search:
+            managers = managers.filter(
+                Q(user__first_name__icontains=manager_search) |
+                Q(user__last_name__icontains=manager_search) |
+                Q(user__email__icontains=manager_search)
+            )
+        
+        # Members Enquiries
         muggles = club.membership_set.filter(is_manager=False)
-        muggle_count = muggles.count()
+        if member_search:
+            muggles = muggles.filter(
+                Q(user__first_name__icontains=member_search) |
+                Q(user__last_name__icontains=member_search) |
+                Q(user__email__icontains=member_search)
+            )
 
         return render(request, "admin_panel/admin_panel_club/members.html", {
             'club_id': club_id,
             'club': club,
             'managers': managers,
-            'muggles' : muggles,
-            'manager_count': manager_count,
-            'muggle_count': muggle_count
+            'muggles': muggles,
+            'user': request.user,
+            'manager_count': managers.count(),
+            'muggle_count': muggles.count(),
+            'manager_search_query': manager_search,
+            'member_search_query': member_search,
         })
     
 class AdminPanelClubsNews(LoginRequiredMixin, UserTypeRequiredMixin, View):
