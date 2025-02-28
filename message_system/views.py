6dddb68850ef502d413fc4d4d8b1d8d3cfc66c11
DavidYouyouWu <k21185211@kcl.ac.uk>
2025-02-28 02:56:14 +0000

    Create message_dashboard

--- message_system/views.py
+++ message_system/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def message_dashboard(request):
-    return render(request, 'message_system/dashboard.html')
+    return render(request, 'messages/message_dashboard.html')
--- student_union/urls.py
+++ student_union/urls.py
@@ -108,6 +108,6 @@ urlpatterns = [
     path("club-dashboard/<int:club_id>/", club_hub.views.club_dashboard, name="club_dashboard"),
 
     # Message related / 消息相关
-    path('messages/', message_system.views.message_dashboard, name='message_dashboard'),
+    path('messages/message_dashboard', message_system.views.message_dashboard, name='message_dashboard'),
 ]   
 
