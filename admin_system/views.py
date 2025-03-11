fdf1ebc3c27736618b87a08cd5d568565dc8a5f9
MingzeLi-qwq <mingzeli.qwq@gmail.com>
2025-03-11 17:54:06 +0000

    add entrance of club hub in the admin panel

--- admin_system/views.py
+++ admin_system/views.py
@@ -142,6 +142,16 @@ class AdminPanelClubsNews(LoginRequiredMixin, UserTypeRequiredMixin, View):
             'club_id':club_id,
         })
     
+class AdminPanelClubsHub(LoginRequiredMixin, UserTypeRequiredMixin, View):
+    allowed_types = ['Admin']
+    def get(self, request, club_id, *args, **kwargs):
+        club = Club.objects.get(pk=club_id)
+        return render(request, "admin_panel/admin_panel_club/hub.html", {
+            'club':club,
+            'club_id':club_id,
+        })
+
+
 class AdminPanelClubsEvents(LoginRequiredMixin, UserTypeRequiredMixin, View):
     allowed_types = ['Admin']
 
