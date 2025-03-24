704de2e44f43175c05f8fa4e81e37068da3443d5
MingzeLi-qwq <mingzeli.qwq@gmail.com>
2025-03-24 15:30:27 +0000

    little fix

--- club_system/models.py
+++ club_system/models.py
@@ -3,9 +3,9 @@ from user_system.models import User
 from django.utils import timezone
 
 class Club(models.Model):
-    club_id = models.PositiveIntegerField(primary_key=True, unique=True, editable=False)  # 从1开始递增的纯数字编号
-    name = models.CharField(max_length=50, unique=True, blank=False)  # 社团名称，不能为空，允许空格, 不可以重复
-    description = models.TextField(default="This Club hasn't added a Description yet", blank=True, null=True)  # 社团简介，无长度限制，默认内容
+    club_id = models.PositiveIntegerField(primary_key=True, unique=True, editable=False)  # Plain numeric numbering in ascending order from 1
+    name = models.CharField(max_length=50, unique=True, blank=False)  # Name of the organization, can not be empty, allow spaces, can not be repeated
+    description = models.TextField(default="This Club hasn't added a Description yet", blank=True, null=True)  # Society profile, no length limit, default content
     members = models.ManyToManyField(
         User,
         through='Membership',
@@ -13,7 +13,6 @@ class Club(models.Model):
     )
 
     """This section is used to implement the logic for incrementing association IDs"""
-    """此部分用来实现社团ID递增的逻辑"""
     def save(self, *args, **kwargs):
         if not self.club_id:  # 当未分配 club_id 时
             last_club = Club.objects.order_by('-club_id').first()
@@ -30,7 +29,6 @@ class Club(models.Model):
 """
 The Membership model is used to represent the many-to-many relationship between users and organizations, 
 and contains additional fields to store the user's role in the organization and the date of joining.
-Membership model用于表现用户和社团间多对多的关系,并包含一些额外的字段来存储用户在社团中的角色和加入日期
 """
 class Membership(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -39,12 +37,12 @@ class Membership(models.Model):
     date_joined = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        unique_together = [('user', 'club')]  # 确保用户不能重复加入同一社团
+        unique_together = [('user', 'club')]  # Ensure that users cannot join the same association repeatedly
 
 
 
 """
-这一部分用来存放用户申请建立新社团的请求
+This section is used to hold user requests to create new clubs
 """
 class NewClubRequest(models.Model):
     STATUS_PENDING = 'pending'
