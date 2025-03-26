12e5605c952a4a5c2942f5674cf932519a34d005
JialeShe <k21151079@kcl.ac.uk>
2025-03-26 21:55:53 +0000

    Fix Clubhub url

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -603,6 +603,7 @@ const Dashboard = () => {
                                         width: '100%',
                                         height: '100%',
                                         objectFit: 'cover',
+                                        overflow: 'hidden'
                                     }}>
                                     <h3>Event Detail</h3>
                                     {widget.data ? (
