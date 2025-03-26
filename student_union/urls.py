328438961888c1c906402756d87838daabf005bb
JialeShe <k21151079@kcl.ac.uk>
2025-03-26 21:27:57 +0000

    Fix Event Selector

--- student_union/urls.py
+++ student_union/urls.py
@@ -94,7 +94,6 @@ urlpatterns = [
 
     path('api/', include('club_hub.urls')),
     path("club-dashboard/<int:club_id>/", club_hub.views.club_dashboard, name="club_dashboard"),
-    path("club-view/<int:club_id>/", club_hub.views.club_dashboard, name="club_dashboard"),
 
     # Message related / 消息相关
     path('messages/message_dashboard', message_system.views.message_dashboard, name='message_dashboard'),
