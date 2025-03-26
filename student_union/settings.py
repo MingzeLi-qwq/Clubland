b25a1a0a48712d2c746a6aa0ccbf3cfbd06c0d7a
JialeShe <k21151079@kcl.ac.uk>
2025-03-26 08:43:30 +0000

    Change CSRF trust address

--- student_union/settings.py
+++ student_union/settings.py
@@ -162,7 +162,12 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+CSRF_TRUSTED_ORIGINS = [
+    "http://51.21.191.188:3000",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    'https://localhost:8000'
+]
 
 if DEBUG:
     # 开发环境下使用本地存储
