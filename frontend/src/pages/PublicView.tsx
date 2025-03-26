f1de8b4aed10c4027d61fed25637352606f106cb
JialeShe <k21151079@kcl.ac.uk>
2025-03-26 21:17:41 +0000

    Fix Event Selector

--- frontend/src/pages/PublicView.tsx
+++ frontend/src/pages/PublicView.tsx
@@ -36,7 +36,6 @@ const PublicView = () => {
     const widgetConfig = {
         clock: { w: 2, h: 3, resizable: false },
         calendar: { w: 3, h: 3, resizable: false },
-        event_selector: { w: 4, h: 4, resizable: true },
         default: { w: 2, h: 2, resizable: true }
     };
     
